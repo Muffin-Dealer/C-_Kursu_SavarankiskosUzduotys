58cd3e00414b980fea8ce3730298987bc6329870
MuffinDealer <55846965+Muffin-Dealer@users.noreply.github.com>
2020-06-08 15:28:35 +0300

    2-3

---
+++ _02Uzduotis/_02Uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Uzduotis
+{
+    //2.Žinomi kambario matmenys –ilgis ir plotis.Abu dydžiai sveikieji skaičiai.Reikia apskaičiuoti, kokią pinigų sumą reiks mokėti už plyteles,
+    // skirtas kambario grindims kloti, jei plytelių vieno kvadrato kaina yra kaina litų. Plytelių reikia pirkti 5 proc.daugiau galimiems nuostoliams padengti
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite kambario ilgi");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite kambario ploti");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite plyteliu kvadrato kaina");
+            double price = Convert.ToDouble(Console.ReadLine());
+            double payment = (lenght * width * 1.05) * price;
+            Console.WriteLine("moketi reikes {0:0.00}", payment);
+
+        }
+    }
+}
---
+++ _03Uzduotis/_03Uzduotis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03Uzduotis
+{
+    //3.Indėlininkas į banką padėjo suma litų. Bankas moka 2proc. metinių palūkanų. 
+    // Sudarykite programą dviejų metų apskaitai skaičiuoti. Ekrane išveskite palūkanas ir turimą indėlį kasmet.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite ideta indeli");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kiek metu laikysite");
+            double years = Convert.ToDouble(Console.ReadLine());
+            double intrest = 0.02;
+            for (int i = 0; i < years; i++)
+            {
+                double intrestSum = deposit* intrest;
+                Console.WriteLine("{0} metu indelis yra {1:0.00} ir palukanu suma yra {2:0.00}", i+1,deposit, intrestSum);
+                deposit += intrestSum;
+            }
+        }
+    }
+}
